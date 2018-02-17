a062eede2d422f96d12b0900edc99cc8107d51c1
Dhanya Jayachandra <dhanyasj01@gmail.com>
2018-02-17 18:59:29 +0000

    fully functional code

--- mousenow.py
+++ mousenow.py
@@ -3,8 +3,9 @@ import pandas
 
 #Variables
 HOME = os.environ['HOME']
+
+#The image is a snip that contains the location of initial element.
 image_loc = HOME +'/Desktop/train_image.png'
-# mouseNow.py - Displays the mouse cursor's current position.
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
@@ -21,36 +22,33 @@ if not co_ordinates:
 #Read the CSV file and get values.
 df = pandas.read_excel('/Users/hithyshikrishnamurthy/Desktop/invoice_details.xlsx')
 (rows, columns) = (df.shape)
-day , month, year = df['Invoice Date'][0].strip().split('/')
-#clicking on the element and passing values
 try:
-   # Find the center of the element located
-   ele_center = pyautogui.center(co_ordinates)
-   pyautogui.click(ele_center)
-   pyautogui.typewrite(day)
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(month)
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(year)
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(str(df['Invoice Number'][0]))
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(str(df['Supplier Name'][0]))
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(str(df['Net Amount'][0]))
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(str(df['VAT'][0]))
-   pyautogui.typewrite('\t')
-   pyautogui.typewrite(str(df['Gross Amount'][0]))
-   pyautogui.typewrite('\t')
+    for i in range(rows):
+       day , month, year = df['Invoice Date'][i].strip().split('/')
+       #clicking on the element and passing values
+       # Find the center of the element located
+       ele_center = pyautogui.center(co_ordinates)
+       pyautogui.click(ele_center)
+       pyautogui.typewrite(day)
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(month)
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(year)
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(str(df['Invoice Number'][i]))
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(str(df['Supplier Name'][i]))
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(str(df['Net Amount'][i]))
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(str(df['VAT'][i]))
+       pyautogui.typewrite('\t')
+       pyautogui.typewrite(str(df['Gross Amount'][i]))
+       pyautogui.typewrite('\t')
    
-   #Submit the form
-   pyautogui.press('enter')
-   positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-
+       #Submit the form
+       pyautogui.press('enter')
 
 except KeyboardInterrupt:
     print('\nDone.')
-print(positionStr, end='')
-print('\b' * len(positionStr), end='', flush=True)
 
