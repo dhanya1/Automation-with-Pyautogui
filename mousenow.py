13d14e47c27e096054c066f020ef0426edfa3fcd
Dhanya Jayachandra <dhanyasj01@gmail.com>
2018-02-17 16:12:16 +0000

    working code pyautogui

--- mousenow.py
+++ mousenow.py
@@ -1,9 +1,10 @@
 import pyautogui, os, sys
-from pandas import read_excel
+import pandas
 
 #Variables
 HOME = os.environ['HOME']
-image_loc = HOME +'/Desktop/train_image'
+image_loc = HOME +'/Desktop/train_image.png'
+# mouseNow.py - Displays the mouse cursor's current position.
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
@@ -17,24 +18,39 @@ if not co_ordinates:
     print('Unable to locate this element on screen. Please ensure the form is open')
     sys.exit()
 
-#Clicking on the element and passing values
+#Read the CSV file and get values.
+df = pandas.read_excel('/Users/hithyshikrishnamurthy/Desktop/invoice_details.xlsx')
+(rows, columns) = (df.shape)
+day , month, year = df['Invoice Date'][0].strip().split('/')
+#clicking on the element and passing values
 try:
    # Find the center of the element located
-   ele_center = pyautogui.center()
-
-   #Click on the center of the element
+   ele_center = pyautogui.center(co_ordinates)
    pyautogui.click(ele_center)
-
+   pyautogui.typewrite(day)
    pyautogui.typewrite('\t')
-   pyautogui.typewrite('Hello world')
+   pyautogui.typewrite(month)
    pyautogui.typewrite('\t')
-   pyautogui.typewrite('Dhanya')
+   pyautogui.typewrite(year)
+   pyautogui.typewrite('\t')
+   pyautogui.typewrite(str(df['Invoice Number'][0]))
+   pyautogui.typewrite('\t')
+   pyautogui.typewrite(str(df['Supplier Name'][0]))
+   pyautogui.typewrite('\t')
+   pyautogui.typewrite(str(df['Net Amount'][0]))
+   pyautogui.typewrite('\t')
+   pyautogui.typewrite(str(df['VAT'][0]))
+   pyautogui.typewrite('\t')
+   pyautogui.typewrite(str(df['Gross Amount'][0]))
+   pyautogui.typewrite('\t')
+   
+   #Submit the form
+   pyautogui.press('enter')
    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
 
-   #Submit the form
-   pyautogui.press('enter') 
 
 except KeyboardInterrupt:
     print('\nDone.')
 print(positionStr, end='')
 print('\b' * len(positionStr), end='', flush=True)
+
